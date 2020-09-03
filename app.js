d0809cc0840536c1bef35af13c5ea675cdd249de
charudutta <restrictedcharudutta@gmail.com>
2020-09-03 19:37:41 +0530

    final commit all fx added

--- app.js
+++ app.js
@@ -9,11 +9,10 @@ const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
 
 function getComputerChoice() {
-    const choice = ['p','r','s'];
+    const choice = ['r','p','s'];
     const randomNo = Math.floor(Math.random()*3);
     return choice[randomNo];
 }
-
 
 function convertToWord(letter) {
     if(letter === "r") return "Rock";
@@ -21,45 +20,55 @@ function convertToWord(letter) {
     else return "Scissors";
 }
 
+const smallUser = "User".fontsize(3).sub();
+const smallComp = "Comp".fontsize(3).sub();
+
+function win(userchoice, compChoice ) {
+    userScore++;
+    userScore_span.innerHTML = userScore;
+    result_div.innerHTML = convertToWord(userchoice) + smallUser + " beats "  + convertToWord(compChoice)+ smallComp +" You win.";
+    document.getElementById(userchoice).classList.add("green-glow");
+    setTimeout(function() {document.getElementById(userchoice).classList.remove("green-glow");}, 500);
+}
+
+function lose(userchoice,compChoice) {
+    compScore++;
+    compScore_span.innerHTML = compScore;
+    result_div.innerHTML = convertToWord(userchoice) + smallUser + " is beaten by " + convertToWord(compChoice)+ smallComp + " You Lose.";
+    document.getElementById(userchoice).classList.add("red-glow");
+    setTimeout(function() {document.getElementById(userchoice).classList.remove("red-glow");}, 500);
+}
+
+function draw(userchoice,compChoice) {
+    result_div.innerHTML = convertToWord(userchoice) + smallUser+ " is same as " + convertToWord(compChoice)+ smallComp + " Its a Draw.";
+    document.getElementById(userchoice).classList.add("grey-glow");
+    setTimeout(function() {document.getElementById(userchoice).classList.remove("grey-glow");}, 500);
+}
 
 
-function game(userchoice) {
+function game(userchoice) { 
     const compChoice = getComputerChoice();
     switch (userchoice + compChoice) {
         case "rs":
         case "pr":
         case "sp":
-            win(userchoice, compChoice);
+            win(userchoice,compChoice);
             break;
         case "rp":
         case "ps":
         case "sr":
-            lose(userchoice, compChoice);
+            lose(userchoice,compChoice);
             break;
         case "pp":
         case "rr":
         case "ss":
-            draw(userchoice, compChoice);
-            break;        
-    }
+            draw(userchoice,compChoice);
+            break;         
+    } 
+
 }
 
-function win(userchoice, compChoice ) {
-    userScore++;
-    userScore_span.innerHTML = userScore;
-    result_div.innerHTML = convertToWord(userchoice) + " beats " + convertToWord(compChoice)+ " You win.";
-    
-}
 
-function lose() {
-    compScore++;
-    compScore_span.innerHTML = compScore;
-    result_div.innerHTML = convertToWord(userchoice) + " is beaten by" + convertToWord(compChoice)+ " You Lose.";
-}
-
-function draw() {
-    result_div.innerHTML = convertToWord(userchoice) + " is same as" + convertToWord(compChoice)+ " Its a Draw.";
-}
 
 function main() {
     rock_div.addEventListener('click', function() {
